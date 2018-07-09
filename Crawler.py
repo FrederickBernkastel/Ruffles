39508bba94826959a3c3f8e7b064ba5864e9a025
Samuel! :) <29274144+6cap-samuel@users.noreply.github.com>
2018-07-09 09:15:29 +0800

    Update Crawler.py
    Recursive calling of crawler done

--- Crawler.py
+++ Crawler.py
@@ -4,8 +4,21 @@ class Crawler:
 	def __init__(self, link, depth):
 		self.link = link
 		self.depth = depth
+		self.contents = []
 
+	def start():
+		self.crawl(self.link, self.depth)
 
-	def crawl():
-		pass
-		
+	def getContents():
+		return self.contents
+
+	def crawl(self, link, depth):
+		if depth < self.depth:
+			scraper = Scraper(link)
+			hyperlinkList = scraper.scrapeLinks()
+			articleDictionary = scraper.scrapeBBCNewsArticle()
+
+			self.append(articleDictionary)
+
+			for link in hyperLinkList:
+				self.crawl(link, 0)
